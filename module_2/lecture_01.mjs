6d70b3d443e395a248f71cff2793634cb5cba5c2
star1010 <anders.singstad@gmail.com>
2024-09-11 12:20:47 +0200

    task 2.1

--- module_2/lecture_01.mjs
+++ module_2/lecture_01.mjs
@@ -1,3 +1,13 @@
 import { initPrintOut, printOut, newLine } from "../../common/script/utils.mjs";
+<<<<<<< HEAD
 initPrintOut(document.getElementById("txtOut"));
 printOut("Test");
+=======
+
+let a = 3;
+(a.toString());
+function plus(){
+
+}
+
+>>>>>>> 45d4a17 (task 2.1)
